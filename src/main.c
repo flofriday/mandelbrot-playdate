946e62966f4706540f0cfbc2f26cfe179a4bfb5e
flofriday <flohacksfriday@gmail.com>
2023-10-08 18:42:46 +0200

    Refactor subdivision code
    Now all parameters about the current render are bundled in a
    struct which makes the code a lot more readable.
    
    This will especially help in the future when more informations are
    needed.

--- src/main.c
+++ src/main.c
@@ -44,6 +44,15 @@ int get_build_time(lua_State *L) {
   return 1;
 }
 
+typedef struct {
+  float start_x;
+  float start_y;
+  float step_x;
+  float step_y;
+  uint8_t *frame;
+
+} RenderParameters;
+
 #define MAX_ITERATIONS 64
 
 inline void draw_pixel(uint8_t *frame, int x, int y) {
@@ -51,10 +60,14 @@ inline void draw_pixel(uint8_t *frame, int x, int y) {
   frame[index / 8] &= (uint8_t)(0xFF7F >> (index % 8));
 }
 
-inline bool calc_inside(int c, int r, float start_x, float start_y,
-                        float step_x, float step_y) {
-  float y0 = start_y + r * step_y + (step_y / 2);
-  float x0 = start_x + c * step_x + (step_x / 2);
+inline bool read_pixel(uint8_t *frame, int x, int y) {
+  int index = x + y * (LCD_COLUMNS + 2 * 8);
+  return frame[index / 8] & (0x80 >> (index % 8));
+}
+
+inline bool calc_inside(int c, int r, RenderParameters params) {
+  float y0 = params.start_y + r * params.step_y + (params.step_y / 2.0f);
+  float x0 = params.start_x + c * params.step_x + (params.step_x / 2.0f);
 
   float x2 = 0.0;
   float y2 = 0.0;
@@ -77,34 +90,32 @@ inline bool calc_inside(int c, int r, float start_x, float start_y,
 
 // At the momment this might recalculate some pixels more than once, if it
 // detects that they are not uniform
-void subdivide_mandelbrot(int x, int y, int n, float start_x, float start_y,
-                          float step_x, float step_y, uint8_t *frame) {
+void subdivide_mandelbrot(int x, int y, int n, RenderParameters params) {
 
   // Check if the border is the same color
-  bool first = calc_inside(x, y, start_x, start_y, step_x, step_y);
+  bool first = calc_inside(x, y, params);
   bool subdivide = false;
   for (int i = 0; i < n - 1; i++) {
     // top, right, bottom, left
-    int t = calc_inside(x + i, y, start_x, start_y, step_x, step_y);
+    bool t = calc_inside(x + i, y, params);
     if (t != first) {
       subdivide = true;
       break;
     }
 
-    int r = calc_inside(x + n - 1, y + i, start_x, start_y, step_x, step_y);
+    bool r = calc_inside(x + n - 1, y + i, params);
     if (r != first) {
       subdivide = true;
       break;
     }
 
-    int b =
-        calc_inside(x + n - 1 - i, y + n - 1, start_x, start_y, step_x, step_y);
+    bool b = calc_inside(x + n - 1 - i, y + n - 1, params);
     if (b != first) {
       subdivide = true;
       break;
     }
 
-    int l = calc_inside(x, y + n - 1 - i, start_x, start_y, step_x, step_y);
+    bool l = calc_inside(x, y + n - 1 - i, params);
     if (l != first) {
       subdivide = true;
       break;
@@ -118,23 +129,19 @@ void subdivide_mandelbrot(int x, int y, int n, float start_x, float start_y,
     if (n < 10) {
       for (int yi = 0; yi < n; yi++) {
         for (int xi = 0; xi < n; xi++) {
-          bool is_black =
-              calc_inside(x + xi, y + yi, start_x, start_y, step_x, step_y);
+          bool is_black = calc_inside(x + xi, y + yi, params);
           if (is_black)
-            draw_pixel(frame, x + xi, y + yi);
+            draw_pixel(params.frame, x + xi, y + yi);
         }
       }
       return;
     }
 
     // top left, top right, bottom left, bottom right
-    subdivide_mandelbrot(x, y, n / 2, start_x, start_y, step_x, step_y, frame);
-    subdivide_mandelbrot(x + n / 2, y, n / 2, start_x, start_y, step_x, step_y,
-                         frame);
-    subdivide_mandelbrot(x, y + n / 2, n / 2, start_x, start_y, step_x, step_y,
-                         frame);
-    subdivide_mandelbrot(x + n / 2, y + n / 2, n / 2, start_x, start_y, step_x,
-                         step_y, frame);
+    subdivide_mandelbrot(x, y, n / 2, params);
+    subdivide_mandelbrot(x + n / 2, y, n / 2, params);
+    subdivide_mandelbrot(x, y + n / 2, n / 2, params);
+    subdivide_mandelbrot(x + n / 2, y + n / 2, n / 2, params);
     return;
   }
 
@@ -144,7 +151,7 @@ void subdivide_mandelbrot(int x, int y, int n, float start_x, float start_y,
 
   for (int iy = 0; iy < n; iy++) {
     for (int ix = 0; ix < n; ix++) {
-      draw_pixel(frame, x + ix, y + iy);
+      draw_pixel(params.frame, x + ix, y + iy);
     }
   }
 }
@@ -157,11 +164,13 @@ int render_mandelbrot(lua_State *L) {
   float step_x = (stopx - start_x) / LCD_COLUMNS;
   float step_y = (stopy - start_y) / LCD_ROWS;
 
-  uint8_t *frame = playdate->graphics->getFrame();
+  RenderParameters params = {
+      start_x, start_y, step_x, step_y, playdate->graphics->getFrame(),
+  };
 
   for (int y = 0; y < LCD_ROWS; y += 80) {
     for (int x = 0; x < LCD_COLUMNS; x += 80) {
-      subdivide_mandelbrot(x, y, 80, start_x, start_y, step_x, step_y, frame);
+      subdivide_mandelbrot(x, y, 80, params);
     }
   }
 
